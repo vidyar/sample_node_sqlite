eb95ff927b9160aff9bc0954562010304eecf418
Vidya Raghavan <vidya@shippable.com>
2015-11-25 17:07:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ describe("Persistence", function () {
       .end(function (e, res) {
         (e === null).should.equal(true);
         var response = res.body;
-        response.should.have.property("name", "doobie");
+        response.should.have.property("name", "doosbie");
         done();
       });
   });
